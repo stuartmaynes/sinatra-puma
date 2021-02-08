9216b9440182627fdb4869a7d7869d51e07dd284
Stuart Maynes <stu@substrakt.com>
2021-02-08 16:38:06 +0000

    UPDATE: Capistrano configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,8 +24,8 @@ ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 # append :linked_files, "config/database.yml"
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-append :linked_dirs, '.bundle'
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", ".bundle"
+# append :linked_dirs, '.bundle'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
